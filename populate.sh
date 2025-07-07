ac5d228ef18f0abfb82824ef403193a721158585
shurik <hilleri123@gmail.com>
2025-07-07 19:16:46 +0300

    Описание изменений

---
+++ populate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+curl -X 'POST' \
+  'http://localhost:8000/auth/register' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "email": "a@aa.ru",
+  "full_name": "AAA",
+  "password": "111111"
+}'
+
+
+curl -X 'POST' \
+  'http://localhost:8000/auth/register' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "email": "b@b.com",
+  "full_name": "BBB",
+  "password": "b"
+}'
+
+
+curl -X 'POST' \
+  'http://localhost:8000/auth/register' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "email": "user@example.com",
+  "full_name": "string",
+  "password": "string"
+}'
+
+
