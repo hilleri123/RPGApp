7892b5c70fc12cee9d110466ff931b12737b25cd
shurik <you@example.com>
2026-01-23 16:13:15 +0300

    Описание изменений

--- plugins/gumshoe/base/ui/src/index.ts
+++ plugins/gumshoe/base/ui/src/index.ts
@@ -4,3 +4,8 @@ export { default as NPCDataView } from './views/NPCDataView';
 export { default as NPCDataEditor } from './editors/NPCDataEditor';
 export { default as ItemDataView } from './views/ItemDataView';
 export { default as ItemDataEditor } from './editors/ItemDataEditor';
+// export { default as LocationDataView } from './views/LocationDataView';
+// export { default as LocationDataEditor } from './editors/LocationDataEditor';
+export { default as ObstacleDataView } from './views/ObstacleDataView';
+export { default as ObstacleDataEditor } from './editors/ObstacleDataEditor';
+
